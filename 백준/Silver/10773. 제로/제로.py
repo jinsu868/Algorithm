acd7d6854c4b1159e7ce8c51365aa45236f91abd
jinsu868 <102043957+jinsu868@users.noreply.github.com>
2023-04-17 18:48:15 +0900

    [Silver IV] Title: 제로, Time: 84 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+st = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        st.pop()
+    else:
+        st.append(x)
+print(sum(st))
