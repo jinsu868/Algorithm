937222016e5c07450ad24b8ec220ec252f1a3efc
JinSu <102043957+jinsu868@users.noreply.github.com>
2023-04-29 19:17:42 +0900

    [Silver III] Title: N과 M (1), Time: 236 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+s = []
+visit = [False] * (n+1)
+
+def dfs():
+    if len(s) == m:
+        for x in s:
+            print(x, end = ' ')
+        print()
+
+    for i in range(1, n+1):
+        if visit[i] == True:
+            continue
+        s.append(i)
+        visit[i] = True
+        dfs()
+        s.pop()
+        visit[i] = False
+
+dfs()
