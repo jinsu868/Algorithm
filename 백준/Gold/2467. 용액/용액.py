265445884ccead35503a95fe7c64bbcf39cbf1e6
JinSu <102043957+jinsu868@users.noreply.github.com>
2023-05-04 16:41:33 +0900

    [Gold V] Title: 용액, Time: 168 ms, Memory: 42176 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l = list(map(int, input().split()))
+
+s = 0
+e = n-1
+res = sys.maxsize
+ans = [-1, -1]
+
+
+while s < e:
+    temp = l[s] + l[e]
+    if abs(res) > abs(temp):
+        res = abs(temp)
+        ans[0] = l[s]
+        ans[1] = l[e]
+
+    elif temp >= 0:
+        e -= 1
+    else:
+        s += 1
+        
+print(ans[0], ans[1])
