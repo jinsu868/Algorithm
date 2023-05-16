e563f0f897f3028098bfe241c8d690b351175b42
JinSu <102043957+jinsu868@users.noreply.github.com>
2023-05-16 19:13:52 +0900

    [Silver II] Title: 연속합, Time: 72 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(1, n):
+    if arr[i-1] > 0:
+        arr[i] = arr[i-1] + arr[i]
+
+print(max(arr))
