8dbea57c4d01b7652f0509794b4ae9506e8886a3
JinSu <102043957+jinsu868@users.noreply.github.com>
2023-05-21 15:13:16 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,15 @@
+def solution(n):
+    res = 0
+    while n != 0:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n -= 1
+            res += 1
+    return res
+
+
+    
+            
+    
+    
