020e6238e14bc54fd91b8600c0f4184e5de748f1
JinSu <102043957+jinsu868@users.noreply.github.com>
2024-01-10 13:30:03 +0900

    [Gold III] Title: 별자리 만들기, Time: 148 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,17 +1,16 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n;
-    static Star[] stars;
     static List<Edge> edges;
+    static List<Star> stars;
     static int[] parents;
-    public static void main(String[] args) throws IOException {
+    static int n;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        stars = new Star[n];
-        edges = new ArrayList<>();
+        stars = new ArrayList<>();
 
         parents = new int[n];
         for (int i = 0; i < n; i++) {
@@ -19,42 +18,71 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             double x = Double.parseDouble(st.nextToken());
             double y = Double.parseDouble(st.nextToken());
-            stars[i] = new Star(i, x, y);
+            stars.add(new Star(x, y));
         }
+        edges = new ArrayList<>();
 
         for (int i = 0; i < n-1; i++) {
             for (int j = i+1; j < n; j++) {
-                Star s1 = stars[i];
-                Star s2 = stars[j];
-                double dist = calcDist(s1, s2);
+                double dist = calculate(stars.get(i), stars.get(j));
                 edges.add(new Edge(i, j, dist));
             }
         }
 
         Collections.sort(edges);
-
-        double res = 0;
         int count = 0;
-        for (Edge edge : edges) {
-            int s = edge.s;
-            int e = edge.e;
-            if (find(s) != find(e)) {
-                count += 1;
-                union(s, e);
-                res += edge.weight;
+        double res = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            Edge edge = edges.get(i);
+            if (find(edge.from) == find(edge.to)) {
+                continue;
             }
+            count++;
+            res += edge.cost;
+            union(edge.from, edge.to);
             if (count == n-1) {
                 break;
             }
         }
-        System.out.printf("%.2f", res);
+        System.out.println(String.format("%.2f", res));
+    }
+
+
+    static double calculate(Star s1, Star s2) {
+        return Math.sqrt(Math.pow(s1.y - s2.y, 2) + Math.pow(s1.x - s2.x, 2));
+    }
+
+    static class Star {
+        double x;
+        double y;
+
+        public Star(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        double cost;
+
+        public Edge(int from, int to, double cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge o) {
+            return (int) (cost - o.cost);
+        }
     }
 
     static int find(int x) {
-        if (x != parents[x]) {
+        if (parents[x] != x) {
             parents[x] = find(parents[x]);
         }
         return parents[x];
@@ -63,54 +91,10 @@ public class Main {
     static void union(int x, int y) {
         x = find(x);
         y = find(y);
-        if (x <= y) {
+        if (x < y) {
             parents[y] = x;
         } else {
             parents[x] = y;
-        }
-    }
-
-
-    private static double calcDist(Star s1, Star s2) {
-        double x1 = s1.x;
-        double x2 = s2.x;
-        double y1 = s1.y;
-        double y2 = s2.y;
-        return Math.sqrt(Math.pow(x2- x1, 2) + Math.pow(y2 - y1, 2));
-    }
-
-    static class Star {
-        int identity;
-        double x;
-        double y;
-        public Star(int identity, double x, double y) {
-            this.identity = identity;
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static class Edge implements Comparable {
-        int s;
-        int e;
-        double weight;
-
-        Edge(int s, int e, double weight) {
-            this.s = s;
-            this.e = e;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            return compareTo((Edge) o);
-        }
-
-        private int compareTo(Edge e) {
-            if (weight >= e.weight) {
-                return 1;
-            }
-            return -1;
         }
     }
 }
